61a8486911f050fe295463276a190b0ce38599b7
d.abdulaxatov <d.abdulaxatov@hamkorbank.uz>
2025-03-06 17:26:42 +0500

    started learning uber's fx project

---
+++ README.md
@@ -0,0 +1,9 @@
+Start a new empty project.
+
+mkdir fxdemo
+cd fxdemo
+```go mod init example.com/fxdemo```
+
+Install the latest version of Fx.
+
+go get go.uber.org/fx@latest
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"net"
+	"net/http"
+
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"go.uber.org/zap"
+)
+
+func main() {
+	fx.New(fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+		return &fxevent.ZapLogger{Logger: log}
+	}),
+		fx.Provide(
+			NewHTTPServer,
+			NewServeMux,
+			NewEchoHandler,
+			zap.NewExample,
+		),
+		fx.Invoke(func(*http.Server) {}),
+	).Run()
+}
+
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux, log *zap.Logger) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+
+			log.Info("Starting HTTP server", zap.String("addr", srv.Addr))
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+
+	return srv
+}
+
+func NewServeMux(echo *EchoHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/echo", echo)
+	return mux
+}
